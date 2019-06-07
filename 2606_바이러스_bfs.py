7717f38f59481aae0ab0c292adc59a21464072c3
MInji <lkjhgff11@naver.com>
2019-06-07 19:32:53 +0900

    bfs기초문제

---
+++ 2606_바이러스_bfs.py
@@ -0,0 +1,24 @@
+from collections import *
+com=int(input())
+n=int(input())
+graph=['@']+[[]for _ in range(com)]
+
+for _ in range(n):
+    y,x=map(int,input().split())
+
+    graph[y].append(x)
+    graph[x].append(y)
+    
+q=deque()
+q.append(graph[1])
+
+visited=[]
+adj=[]
+while q:
+    adj=q.popleft()
+
+    for i in adj:
+        if i in visited:
+            continue
+        visited.append(i)
+        q.append(graph[i])
