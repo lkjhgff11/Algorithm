cf58ae3dcb504168919f5313de611065e8530b8c
ruru <lkjhgff11@naver.com>
2019-08-13 14:46:53 +0900

    16957 체스판위의 공
    재귀 , dp

--- 16957 체스판 위의 공.py
+++ 16957 체스판 위의 공.py
@@ -1,48 +1,44 @@
-from collections import deque
-dy = [-1,-1,-1,0,1,1,1,0]
-dx = [-1,0,1,1,1,0,-1,-1]
-
 r,c = map(int,input().split())
 grid = [list(map(int,input().split()))for _ in range(r)]
 
-ball = [[1]*c for _ in range(r)]
+root = [[(-1,-1)]*c for _ in range(r)]
 
+# y,x의 루트를 줭
+def rec(y,x):
+    if root[y][x] != (-1,-1):
+        return root[y][x]
 
-while True:
-    before_ball = [b[:]for b in ball]
-    for y in range(r):
-        for x in range(c):       
-            balls = []
-            minn = 10 ** 10
-            for i in range(8):
-                ny = y+dy[i]
-                nx = x+dx[i]
+    minn = (10**10,0,0)
+    for dy in range(-1,2):
+        for dx in range(-1,2):
+            ny = y + dy
+            nx = x + dx
 
-                if ny in (-1,r) or nx in (-1,c):
-                    continue
+            if ny in (-1,r) or nx in (-1,c):
+                continue
+            minn = min(minn,(grid[ny][nx],ny,nx))
 
-                if grid[y][x] < grid[ny][nx]:
-                    continue    
+   
+    minn = minn[1:] 
+    if minn == (y,x):
+        root[y][x] = (y,x)
+        return root[y][x]
+     
 
-                if grid[y][x] > grid[ny][nx] and ball[y][x] != 0:
-                    balls.append((ny,nx))
-
-                
-            # 가장 작은 정수칸 이동
-            if len(balls) > 0:    
-                for (a,b) in balls:
-                    if minn > grid[a][b]:
-                        minn = grid[a][b]
-                        minn_xy = (a,b)
-                
-                ball[y][x] -= 1
-                ball[minn_xy[0]][minn_xy[1]] += 1
-
+    else:
+        (ny,nx) = minn
+        root[y][x] = rec(ny,nx)
+        return root[y][x]
     
-    if before_ball == ball:
-        break
-               
-for i in range(r):
-    for j in range(c):
-        print(ball[i][j],end=" ")
-    print()            
+for y in range(r):
+    for x in range(c):
+        rec(y,x)
+
+ans = [[0]*c for _ in range(r)]
+for y in range(r):
+    for x in range(c):
+        ans_y,ans_x = root[y][x]
+        ans[ans_y][ans_x] += 1
+    
+for a in ans:
+    print(*a)
